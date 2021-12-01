d1a852c28c04617b61e33cb8f98084ab3c82f0c1
James Cole <james@assimityai.com>
2021-12-01 07:59:07 +0000

    Add approach using windows

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -7,6 +7,7 @@ fn main() -> Result<(), Error> {
     println!("Hello day_01!");
     part_01()?;
     part_02()?;
+    part_02_window()?;
     Ok(())
 }
 
@@ -62,5 +63,28 @@ fn part_02() -> Result<(), Error> {
         }
     }
     println!("Part 2: aggregated increases: {}", increase_count);
+    Ok(())
+}
+
+fn part_02_window() -> Result<(), Error> {
+    let depth = File::open("day_01/input/depths.txt")?;
+    let buffer = BufReader::new(depth);
+    let mut depths: Vec<u16> = vec![];
+    for line in buffer.lines() {
+        depths.push(line.unwrap().parse::<u16>().unwrap());
+    }
+
+    let mut prev_window = 0;
+    let mut increases = 0;
+    // Considered using iterators all the way but we're doing side-effects
+    // in prev_window, so a for loop is more idiomatic.
+    for window in depths.windows(3) {
+        let current_window: u16 = window.iter().sum();
+        if prev_window != 0 && current_window > prev_window {
+            increases += 1;
+        }
+        prev_window = current_window;
+    }
+    println!("Part 2: aggregated increases using windows: {}", increases);
     Ok(())
 }
